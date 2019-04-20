4327dfbfff2f65864d0e4641b91ddbea05850093
Willy Camargo <camargowilly@gmail.com>
2019-04-20 17:35:15 -0300

    add Button component with initial setup

--- src/lib/components/Button/index.js
+++ src/lib/components/Button/index.js
@@ -1,104 +1,47 @@
 import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom'
 import style from './style.module.css'
 
-const Link = 'a'
-
-const Button = ({
+const Button = ({ className, variant, color, size, disabled, children, Element, ...rest }) => {
+  const buttonClasses = classNames(
     className,
-    onClick,
-    variant,
-    color,
-    size,
-    children,
-    type,
-    to,
-    disabled,
-}) => {
-    const buttonClasses = classNames(
-        className,
-        style.button,
-        style[variant],
-        style[color],
-        style[size],
-        { [style.disabled]: disabled }
-    )
+    style.button,
+    style[variant],
+    style[color],
+    style[size],
+    { [style.disabled]: disabled }
+  )
 
-    if (type === 'link') {
-        if (to.indexOf('http') !== -1 || to.indexOf('www') !== -1) {
-            return (
-                <a
-                    href={to}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={buttonClasses}
-                >
-                    {children}
-                </a>
-            )
-        }
-
-        return (
-            <Link
-                className={buttonClasses}
-                onClick={onClick}
-                to={to}
-            >
-                {children}
-            </Link>
-        )
-    }
-
-    return (
-        <button
-            className={buttonClasses}
-            onClick={onClick}
-            type={type}
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    )
+  return (
+    <Element className={buttonClasses} disabled={disabled} {...rest}>
+      {children}
+    </Element>
+  )
 }
 
 Button.propTypes = {
-    className: PropTypes.string,
-    type: PropTypes.oneOf(['button', 'submit', 'reset', 'link']),
-    to: PropTypes.string,
-    onClick: PropTypes.func,
-    variant: PropTypes.oneOf([
-        'flat',
-        'outline',
-        'dashed',
-        'clean',
-        'block',
-        'icon',
-    ]),
-    color: PropTypes.oneOf([
-        'primary',
-        'secondary',
-        'tertiary',
-        'quartenary',
-    ]),
-    size: PropTypes.oneOf(['micro', 'tiny', 'small', 'medium', 'large']),
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.string,
-        PropTypes.node,
-    ]).isRequired,
-    disabled: PropTypes.bool,
+  className: PropTypes.string,
+  Element: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  variant: PropTypes.oneOf(['flat', 'outline', 'dashed', 'clean', 'icon']),
+  color: PropTypes.oneOf(['primary', 'secondary', 'neutral', 'error', 'alert', 'success']),
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.string,
+    PropTypes.node,
+  ]).isRequired,
+  disabled: PropTypes.bool,
 }
 
 Button.defaultProps = {
-    className: null,
-    onClick: null,
-    variant: 'flat',
-    color: 'primary',
-    size: 'small',
-    type: 'button',
-    disabled: false,
+  className: null,
+  onClick: null,
+  variant: 'flat',
+  color: 'primary',
+  size: 'md',
+  disabled: false,
+  Element: 'button',
 }
 
 export default Button
