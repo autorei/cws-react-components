63551da7fe8e7ac654d3a8c5d33c14462785f39f
Willy Camargo <camargowilly@gmail.com>
2019-04-20 17:36:04 -0300

    add: Button histories and update stylelint

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
-import { Button } from '../src/lib';
+import { Button } from '../src/lib'
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
+  .add('Colors', () => (
+    <>
+      <Button color="primary">Primary Color</Button>
+      <Button color="secondary">Secondary Color</Button>
+      <Button color="neutral">Neutral Color</Button>
+      <Button color="error">Error Color</Button>
+      <Button color="alert">Alert Color</Button>
+      <Button color="success">Success Color</Button>
+    </>
+  ))
+  .add('Sizes', () => (
+    <>
+      <Button size="lg">LG Button</Button>
+      <Button size="md">MD Button</Button>
+      <Button size="sm">SM Button</Button>
+    </>
+  ))
+  .add('Emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
+  ))
